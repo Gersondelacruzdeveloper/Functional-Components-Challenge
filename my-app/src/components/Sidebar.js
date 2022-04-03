0594d5d4ecee9603988b0ee1f3990cc6d142f2c0
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-04-03 09:09:27 +0100

    Add content on sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <a>My photos </a>
+      <a>My illustrations </a>
+      <a>My Paintings </a>
+    </div>
+  )
+}
+
+export default Sidebar
